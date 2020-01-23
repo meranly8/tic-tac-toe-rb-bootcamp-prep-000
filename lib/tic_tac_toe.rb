34b0a73da0731878e5ad6a927b71801bb1235a68
Michelle Ranly <meranly8@gmail.com>
2020-01-23 15:11:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,8 +29,8 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " " || board[index] == "")
 end
 
-def valid_move?(board, position)
-  position.between?(0,8) && !position_taken?(board, position)
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, position)
 end
 
 def turn(board)
